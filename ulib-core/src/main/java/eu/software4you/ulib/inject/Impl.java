d6cb3a20f02d9723a45de6f58f4e412b1dbf5a0c
fluse1367 <3984402-fluse1367@users.noreply.gitlab.com>
2021-02-10 12:18:28 +0100

    some javadocs

--- ulib-core/src/main/java/eu/software4you/ulib/inject/Impl.java
+++ ulib-core/src/main/java/eu/software4you/ulib/inject/Impl.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Short for implementation. A new instance of the class will be injected into the target class.
+ * Target class needs a static field that is annotated with {@link eu.software4you.ulib.Await}.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Impl {
--- ulib-core/src/main/java/eu/software4you/ulib/inject/ImplConst.java
+++ ulib-core/src/main/java/eu/software4you/ulib/inject/ImplConst.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Short for implementation constructor.
+ * Will be injected as {@link eu.software4you.function.ConstructingFunction} into the target.
+ * Target class needs a static {@link eu.software4you.function.ConstructingFunction} field
+ * that is annotated with {@link eu.software4you.ulib.Await}.
+ */
 @Target(ElementType.CONSTRUCTOR)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ImplConst {
