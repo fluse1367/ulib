83ff0eb62608b43ff5c4e262dddf00797e6aef7f
fluse1367 <fluse1367@software4you.eu>
2021-04-23 15:52:23 +0200

    UUIDFetcher -> HttpUtil

--- ulib-spigot-api/src/main/java/eu/software4you/spigot/UUIDFetcher.java
+++ ulib-spigot-api/src/main/java/eu/software4you/spigot/UUIDFetcher.java
@@ -1,17 +1,14 @@
 package eu.software4you.spigot;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
+import eu.software4you.http.HttpUtil;
+
 import java.util.UUID;
 
 public class UUIDFetcher {
 
     public static UUID getUUID(final String playername) {
         try {
-            final String output = callURL("https://api.mojang.com/users/profiles/minecraft/" + playername);
+            final String output = HttpUtil.getContentAsString("https://api.mojang.com/users/profiles/minecraft/" + playername);
             final StringBuilder result = new StringBuilder();
             readData(output, result);
             final String u = result.toString();
@@ -32,33 +29,5 @@ public class UUIDFetcher {
         for (int i = 7; i < 200 && !String.valueOf(toRead.charAt(i)).equalsIgnoreCase("\""); ++i) {
             result.append(toRead.charAt(i));
         }
-    }
-
-    private static String callURL(final String URL) {
-        final StringBuilder sb = new StringBuilder();
-        URLConnection urlConn = null;
-        InputStreamReader in = null;
-        try {
-            final URL url = new URL(URL);
-            urlConn = url.openConnection();
-            if (urlConn != null) {
-                urlConn.setReadTimeout(60000);
-            }
-            if (urlConn != null && urlConn.getInputStream() != null) {
-                in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
-                final BufferedReader bufferedReader = new BufferedReader(in);
-                if (bufferedReader != null) {
-                    int cp;
-                    while ((cp = bufferedReader.read()) != -1) {
-                        sb.append((char) cp);
-                    }
-                    bufferedReader.close();
-                }
-            }
-            in.close();
-        } catch (Exception e) {
-            return null;
-        }
-        return sb.toString();
     }
 }
