e6fc2c741d9c8c688e0f40acf8d235a733da070d
fluse1367 <fluse1367@software4you.eu>
2021-05-25 01:15:23 +0200

    fix spacing

--- ulib-core/src/main/java/eu/software4you/ulib/impl/database/sql/Where.java
+++ ulib-core/src/main/java/eu/software4you/ulib/impl/database/sql/Where.java
@@ -16,7 +16,7 @@ final class Where extends QueryEndpoint implements eu.software4you.database.sql.
 
     Where(SqlDatabase sql, StringBuilder query, String condition) {
         super(sql, query);
-        append("where", condition);
+        append(" where", condition);
     }
 
     private void append(String operand, String condition) {
@@ -31,12 +31,12 @@ final class Where extends QueryEndpoint implements eu.software4you.database.sql.
 
     @Override
     public Condition<eu.software4you.database.sql.query.Where> and(String column) {
-        return new Condition<>(sql, query, column, c -> new Where(c, "and"));
+        return new Condition<>(sql, query, column, c -> new Where(c, " and"));
     }
 
     @Override
     public Where andRaw(String condition) {
-        append("and", condition);
+        append(" and", condition);
         return this;
     }
 
@@ -47,12 +47,12 @@ final class Where extends QueryEndpoint implements eu.software4you.database.sql.
 
     @Override
     public Condition<eu.software4you.database.sql.query.Where> or(String column) {
-        return new Condition<>(sql, query, column, c -> new Where(c, "or"));
+        return new Condition<>(sql, query, column, c -> new Where(c, " or"));
     }
 
     @Override
     public Where orRaw(String condition) {
-        append("or", condition);
+        append(" or", condition);
         return this;
     }
 }
