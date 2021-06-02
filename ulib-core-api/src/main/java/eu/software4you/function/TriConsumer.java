a21f66b80cce1b1fdf2b741fc9390874c04520d3
fluse1367 <fluse1367@software4you.eu>
2021-06-02 10:08:55 +0200

    javadoc

--- ulib-core-api/src/main/java/eu/software4you/function/TriConsumer.java
+++ ulib-core-api/src/main/java/eu/software4you/function/TriConsumer.java
@@ -1,6 +1,22 @@
 package eu.software4you.function;
 
+/**
+ * Just like a {@link java.util.function.BiConsumer} but with 1 additional argument.
+ *
+ * @param <A> first argument type
+ * @param <B> second argument type
+ * @param <C> third argument type
+ * @see java.util.function.BiConsumer
+ * @see java.util.function.Consumer
+ */
 @FunctionalInterface
 public interface TriConsumer<A, B, C> {
+    /**
+     * Applies the operation to the given arguments.
+     *
+     * @param a the first argument
+     * @param b the second argument
+     * @param c the third argument
+     */
     void apply(A a, B b, C c);
 }
--- ulib-core-api/src/main/java/eu/software4you/function/TriFunction.java
+++ ulib-core-api/src/main/java/eu/software4you/function/TriFunction.java
@@ -1,6 +1,24 @@
 package eu.software4you.function;
 
+/**
+ * Just like a {@link java.util.function.BiFunction} but with 1 additional argument.
+ *
+ * @param <A> first argument type
+ * @param <B> second argument type
+ * @param <C> third argument type
+ * @param <R> return type
+ * @see java.util.function.BiFunction
+ * @see java.util.function.Function
+ */
 @FunctionalInterface
 public interface TriFunction<A, B, C, R> {
+    /**
+     * Applies the function to the given arguments.
+     *
+     * @param a the first argument
+     * @param b the second argument
+     * @param c the third argument
+     * @return the result
+     */
     R apply(A a, B b, C c);
 }
