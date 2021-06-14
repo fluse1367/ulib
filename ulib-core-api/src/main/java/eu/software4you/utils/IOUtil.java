7867d856d1d91c420e10bfadd5d0c7a466251b24
fluse1367 <fluse1367@software4you.eu>
2021-06-14 19:31:55 +0200

    reader/writer operations

--- ulib-core-api/src/main/java/eu/software4you/utils/IOUtil.java
+++ ulib-core-api/src/main/java/eu/software4you/utils/IOUtil.java
@@ -3,10 +3,7 @@ package eu.software4you.utils;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class IOUtil {
     /**
@@ -14,7 +11,7 @@ public class IOUtil {
      *
      * @param in  the stream to read from
      * @param out the stream to write to
-     * @throws IOException inherited from {@link InputStream#read()}, {@link InputStream#close()} ()},
+     * @throws IOException inherited from {@link InputStream#read()}, {@link InputStream#close()},
      *                     {@link OutputStream#write(byte[], int, int)}, {@link OutputStream#flush()} and {@link OutputStream#close()}
      * @see InputStream#read()
      * @see InputStream#close()
@@ -34,6 +31,30 @@ public class IOUtil {
     }
 
     /**
+     * Writes an reader into a writer. This method closes the objects.
+     *
+     * @param reader the reader to read from
+     * @param writer the writer to write to
+     * @throws IOException inherited from {@link Reader#read()}, {@link Reader#close()},
+     *                     {@link Writer#write(char[], int, int)}, {@link Writer#flush()} and {@link Writer#close()}
+     * @see InputStream#read()
+     * @see InputStream#close()
+     * @see OutputStream#write(byte[], int, int)
+     * @see OutputStream#flush()
+     * @see OutputStream#close()
+     */
+    public static void write(@NotNull Reader reader, @NotNull Writer writer) throws IOException {
+        try (val in = reader; val out = writer) {
+            char[] buff = new char[1024];
+            int len;
+            while ((len = in.read(buff)) != -1) {
+                out.write(buff, 0, len);
+            }
+            out.flush();
+        }
+    }
+
+    /**
      * Reads all bytes from an input stream. This method closes the stream.
      *
      * @param in the stream to read from
@@ -45,6 +66,20 @@ public class IOUtil {
         val bout = new ByteArrayOutputStream();
         write(in, bout);
         return bout.toByteArray();
+    }
+
+    /**
+     * Reads all chars from an reader.
+     *
+     * @param reader the reader to read from
+     * @return the bytes read
+     * @throws IOException inherited from {@link #write(Reader, Writer)}
+     * @see #write(InputStream, OutputStream)
+     */
+    public static char[] read(@NotNull Reader reader) throws IOException {
+        val cout = new CharArrayWriter();
+        write(reader, cout);
+        return cout.toCharArray();
     }
 
     /**
