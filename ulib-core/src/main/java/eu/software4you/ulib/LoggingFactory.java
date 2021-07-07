fluse1367/ulib

commit b9ec123f972c5bc5dbd7f63d53a154a657aa677f

ansi update

ulib-core/src/main/java/eu/software4you/ulib/LoggingFactory.java
@@ -1,5 +1,8 @@
 package eu.software4you.ulib;
 
+import lombok.val;
+import org.fusesource.jansi.AnsiConsole;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -7,41 +10,46 @@ import java.io.StringWriter;
 import java.util.Date;
 import java.util.logging.*;
 
+import static org.fusesource.jansi.Ansi.Color.*;
+import static org.fusesource.jansi.Ansi.ansi;
+
 class LoggingFactory {
     private final Properties properties;
-    private final Logger logger;
     private final Lib instance;
+    private final Logger logger;
 
-    private LoggingFactory(Properties properties, Logger logger, Lib instance) {
+    private LoggingFactory(Properties properties, Lib instance) {
         this.properties = properties;
-        this.logger = logger;
         this.instance = instance;
+
+        this.logger = Logger.getLogger(instance.getClass().getName());
+        this.logger.setUseParentHandlers(false);
     }
 
     static Logger fabricate(Properties properties, LibImpl instance) {
-        Logger logger = Logger.getLogger(instance.getClass().getName());
-        logger.setUseParentHandlers(false);
+        LoggingFactory factory = new LoggingFactory(properties, instance);
+        factory.terminalInit();
+        factory.fileInit();
+        factory.logger.setLevel(properties.LOG_LEVEL);
+        AnsiConsole.systemInstall();
 
-        LoggingFactory factory = new LoggingFactory(properties, logger, instance);
-        factory.prepare();
-        factory.systemInstall();
-
-        return logger;
+        return factory.logger;
     }
 
-    private void prepare() {
-        // init logger
+    private void terminalInit() {
         PrintStream err = System.err;
         // make ConsoleHandler use the actual stdout
         System.setErr(System.out);
         ConsoleHandler consoleHandler = new ConsoleHandler();
-        // reset System.out to previous one
+        // reset System.err to previous one
         System.setErr(err);
 
         consoleHandler.setFormatter(ansiFormatter());
         consoleHandler.setLevel(properties.LOG_LEVEL);
         logger.addHandler(consoleHandler);
+    }
 
+    private void fileInit() {
         try {
             if (!properties.DATA_DIR.exists()) {
                 if (!properties.DATA_DIR.mkdirs()) {
@@ -58,11 +66,6 @@ class LoggingFactory {
             System.err.println("Could not append file handler to logger. All uLib logged records will not be saved to disk.");
             e.printStackTrace();
         }
-        logger.setLevel(properties.LOG_LEVEL);
-    }
-
-    private void systemInstall() {
-        org.fusesource.jansi.AnsiConsole.systemInstall();
     }
 
     private Formatter normalFormatter() {
@@ -81,23 +84,26 @@ class LoggingFactory {
         return new Formatter() {
             @Override
             public String format(LogRecord record) {
-                StringBuilder b = new StringBuilder();
-
+                val ansi = ansi();
                 Level level = record.getLevel();
 
                 if (level == Level.SEVERE) {
-                    b.append(Escapes.RED);
+                    ansi.fg(RED);
                 } else if (level == Level.WARNING) {
-                    b.append(Escapes.YELLOW);
+                    ansi.fg(YELLOW);
+                } else if (level == Level.FINE) {
+                    ansi.fg(BLUE);
+                } else if (level == Level.FINER) {
+                    ansi.fg(CYAN);
+                } else if (level == Level.FINEST) {
+                    ansi.fg(MAGENTA);
                 }
 
-                b.append(simpleFormat(record));
+                ansi.a(simpleFormat(record));
 
                 if (record.getThrown() != null)
-                    b.append(Escapes.RED)
-                            .append(getStackTrace(record.getThrown())).append("\n");
-                b.append(Escapes.RESET);
-                return b.toString();
+                    ansi.fg(RED).a(getStackTrace(record.getThrown()) + "\n");
+                return ansi.reset().toString();
             }
         };
     }
@@ -114,22 +120,5 @@ class LoggingFactory {
         StringWriter wr = new StringWriter();
         throwable.printStackTrace(new PrintWriter(wr));
         return wr.toString();
-    }
-
-    private static class Escapes {
-        private static final String ESC = "\033";
-
-        // Reset all escapes
-        private static final String RESET = ESC + "[0m";
-
-        // Regular Colors
-        private static final String BLACK = ESC + "[0;30m";
-        private static final String RED = ESC + "[0;31m";
-        private static final String GREEN = ESC + "[0;32m";
-        private static final String YELLOW = ESC + "[0;33m";
-        private static final String BLUE = ESC + "[0;34m";
-        private static final String PURPLE = ESC + "[0;35m";
-        private static final String CYAN = ESC + "[0;36m";
-        private static final String WHITE = ESC + "[0;37m";
     }
 }

commit 43ee17a5e0c92ebd809f25f06dce1c2641816886

bukkit ansi color compatibility (bug introduced by b9ec123f)

ulib-core/src/main/java/eu/software4you/ulib/LoggingFactory.java
@@ -68,8 +68,8 @@ class LoggingFactory {
             public String format(LogRecord record) {
                 StringBuilder b = new StringBuilder(simpleFormat(record));
                 if (record.getThrown() != null)
-                    b.append(getStackTrace(record.getThrown())).append("\n");
-                return b.toString();
+                    b.append("\n").append(getStackTrace(record.getThrown()));
+                return b.append("\n").toString();
             }
         };
     }
@@ -96,8 +96,8 @@ class LoggingFactory {
                 ansi.a(simpleFormat(record));
 
                 if (record.getThrown() != null)
-                    ansi.fg(RED).a(getStackTrace(record.getThrown()) + "\n");
-                return ansi.reset().toString();
+                    ansi.a("\n").fg(RED).a(getStackTrace(record.getThrown()));
+                return ansi.reset().a("\n").toString();
             }
         };
     }
@@ -106,7 +106,7 @@ class LoggingFactory {
         long tid = record.getThreadID();
         String thread = tid == LibImpl.MAIN_THREAD_ID ? "main" : String.format("t-%d", tid);
         String prefix = String.format("[%s/%s]", instance.getNameOnly(), thread);
-        return String.format("%-12s %tT %s: %s\n", prefix,
+        return String.format("%-12s %tT %s: %s", prefix,
                 new Date(record.getMillis()), record.getLevel().getName(), record.getMessage());
     }
 