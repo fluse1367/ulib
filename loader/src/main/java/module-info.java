fc2861b44fef89b010cb898f05ccb4017d4eaf28
fluse1367 <fluse1367@software4you.eu>
2021-12-16 22:44:01 +0100

    compatibility

--- loader/src/main/java/module-info.java
+++ loader/src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module ulib.loader {
     requires static lombok;
 
+    requires static ulib.core.api;
+    requires static ulib.core;
+    requires static joptsimple;
+
     requires java.instrument;
     requires java.logging;
     requires jdk.attach;
 
-    requires ulib.core.api;
-    requires ulib.core;
-    requires joptsimple;
-
     opens eu.software4you.ulib.loader.install;
+    exports eu.software4you.ulib.loader.agent to java.instrument;
 }
