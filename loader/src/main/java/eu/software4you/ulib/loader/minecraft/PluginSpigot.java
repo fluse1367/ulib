44490b1d8b7b6212b866bd658cc62ff5f6d77739
fluse1367 <fluse1367@software4you.eu>
2022-01-18 23:40:48 +0100

    /ulib command

--- loader/src/main/java/eu/software4you/ulib/loader/minecraft/PluginSpigot.java
+++ loader/src/main/java/eu/software4you/ulib/loader/minecraft/PluginSpigot.java
@@ -1,7 +1,11 @@
 package eu.software4you.ulib.loader.minecraft;
 
+import eu.software4you.ulib.core.ULib;
 import eu.software4you.ulib.loader.install.provider.EnvironmentProvider;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
 
 public class PluginSpigot extends JavaPlugin {
     static {
@@ -11,5 +15,15 @@ public class PluginSpigot extends JavaPlugin {
     @Override
     public void onLoad() {
         Init.init(getClass());
+    }
+
+    @Override
+    public void onEnable() {
+        Optional.ofNullable(getCommand("ulib"))
+                .orElseThrow()
+                .setExecutor((sender, command, label, args) -> {
+                    sender.sendMessage("%suLib version %s".formatted(ChatColor.GREEN, ULib.get().getVersion()));
+                    return true;
+                });
     }
 }
