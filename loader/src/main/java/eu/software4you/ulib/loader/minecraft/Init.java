3c56b86b3c053d8607accacdfd9b6c1251ac58e8
fluse1367 <fluse1367@software4you.eu>
2022-02-13 18:18:15 +0100

    fail-proof init

--- loader/src/main/java/eu/software4you/ulib/loader/minecraft/Init.java
+++ loader/src/main/java/eu/software4you/ulib/loader/minecraft/Init.java
@@ -4,9 +4,14 @@ import eu.software4you.ulib.loader.install.Installer;
 
 final class Init {
     static void init(Class<?> cl) {
-        Installer.installTo(cl.getClassLoader());
+        try {
+            Installer.installTo(cl.getClassLoader());
+        } catch (IllegalArgumentException e) {
+            // loaders were already installed to class loader!
+        }
         var module = cl.getModule();
-        if (module.isNamed())
-            module.addReads(Installer.getModule());
+        var other = Installer.getModule();
+        if (module.isNamed() && !module.canRead(other))
+            module.addReads(other);
     }
 }
