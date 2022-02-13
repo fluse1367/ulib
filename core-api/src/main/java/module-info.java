e0ce9a48a2565fca13fbe5f3894a726ae5d9ab3d
fluse1367 <fluse1367@software4you.eu>
2022-02-13 16:14:27 +0100

    export

--- core-api/src/main/java/module-info.java
+++ core-api/src/main/java/module-info.java
@@ -17,24 +17,27 @@ module ulib.core.api {
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
 
-    exports eu.software4you.ulib.core;
-    exports eu.software4you.ulib.core.api;
-    exports eu.software4you.ulib.core.api.common;
     exports eu.software4you.ulib.core.api.common.collection;
-    exports eu.software4you.ulib.core.api.configuration;
+    exports eu.software4you.ulib.core.api.common.validation;
+    exports eu.software4you.ulib.core.api.common;
     exports eu.software4you.ulib.core.api.configuration.serialization;
     exports eu.software4you.ulib.core.api.configuration.yaml;
-    exports eu.software4you.ulib.core.api.database;
-    exports eu.software4you.ulib.core.api.database.sql;
+    exports eu.software4you.ulib.core.api.configuration;
+    exports eu.software4you.ulib.core.api.database.exception;
     exports eu.software4you.ulib.core.api.database.sql.query;
+    exports eu.software4you.ulib.core.api.database.sql;
+    exports eu.software4you.ulib.core.api.database;
     exports eu.software4you.ulib.core.api.dependencies;
+    exports eu.software4you.ulib.core.api.function;
     exports eu.software4you.ulib.core.api.http;
     exports eu.software4you.ulib.core.api.io;
+    exports eu.software4you.ulib.core.api.math;
     exports eu.software4you.ulib.core.api.reflect;
     exports eu.software4you.ulib.core.api.sql;
     exports eu.software4you.ulib.core.api.transform;
     exports eu.software4you.ulib.core.api.utils;
-    exports eu.software4you.ulib.core.api.math;
+    exports eu.software4you.ulib.core.api;
+    exports eu.software4you.ulib.core;
 
     exports eu.software4you.ulib.core.api.internal to
             ulib.core, ulib.minecraft.api, ulib.bungeecord.api, ulib.velocity.api, ulib.spigot.api;
