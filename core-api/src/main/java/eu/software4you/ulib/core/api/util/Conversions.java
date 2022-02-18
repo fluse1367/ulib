77205d4938097e352d1b86edb9b25544b274aec5
fluse1367 <fluse1367@software4you.eu>
2022-02-18 21:44:09 +0100

    simple conversions

---
+++ core-api/src/main/java/eu/software4you/ulib/core/api/util/Conversions.java
@@ -0,0 +1,74 @@
+package eu.software4you.ulib.core.api.util;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Conversions {
+
+    /**
+     * Tries to convert the given object into an int.
+     *
+     * @param o the value to convert
+     * @return an optional wrapping the value if conversation was successful, an empty optional otherwise
+     */
+    public static Optional<Integer> tryInt(Object o) {
+        return tryConvert(o, in -> in instanceof Integer i ? i : Integer.parseInt(in.toString()));
+    }
+
+    /**
+     * Tries to convert the given object into a long.
+     *
+     * @param o the value to convert
+     * @return an optional wrapping the value if conversation was successful, an empty optional otherwise
+     */
+    public static Optional<Long> tryLong(Object o) {
+        return tryConvert(o, in -> in instanceof Long l ? l : Long.parseLong(in.toString()));
+    }
+
+    /**
+     * Tries to convert the given object into a float.
+     *
+     * @param o the value to convert
+     * @return an optional wrapping the value if conversation was successful, an empty optional otherwise
+     */
+    public static Optional<Float> tryFloat(Object o) {
+        return tryConvert(o, in -> in instanceof Float f ? f : Float.parseFloat(in.toString()));
+    }
+
+    /**
+     * Tries to convert the given object into a double.
+     *
+     * @param o the value to convert
+     * @return an optional wrapping the value if conversation was successful, an empty optional otherwise
+     */
+    public static Optional<Double> tryDouble(Object o) {
+        return tryConvert(o, in -> in instanceof Double d ? d : Double.parseDouble(in.toString()));
+    }
+
+    /**
+     * Tries to convert the given object into a boolean.
+     * If the object is applicable as integer (according to {@link #tryInt(Object)}), a value other than 0 will be interpreted as true.
+     *
+     * @param o the value to convert
+     * @return an optional wrapping the value if conversation was successful, an empty optional otherwise
+     */
+    public static Optional<Boolean> tryBoolean(@Nullable Object o) {
+        return tryConvert(o, in -> in instanceof Boolean b ? b :
+                tryInt(in).map(i -> i != 0).orElseGet(() -> Boolean.parseBoolean(o.toString())));
+    }
+
+    /**
+     * Tries to convert the specified object to another object. Any object thrown will be caught.
+     *
+     * @param input     the object to convert
+     * @param converter the converting function
+     * @return an optional wrapping the value if the converting function executed successful and returned a non-null value, an empty optional otherwise
+     */
+    public static <I, R> Optional<R> tryConvert(@Nullable I input, @Nullable Function<I, R> converter) {
+        if (Checks.nil(input, converter))
+            return Optional.empty();
+        return Unsettled.execute(() -> converter.apply(input)).get();
+    }
+}
---
+++ core-api/src/main/java/eu/software4you/ulib/core/api/util/Unsettled.java
@@ -0,0 +1,93 @@
+package eu.software4you.ulib.core.api.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * A result of an execution.
+ *
+ * @param <T> the type of resulting value
+ */
+public class Unsettled<T> {
+
+    private static final Unsettled<?> NONE = new Unsettled<>(null, null);
+
+    /**
+     * Constructs a new unsettled object with a successful result.
+     *
+     * @param value the value to hold, may be null
+     * @param <T>   the type of value
+     * @return the newly constructed object, or the static empty unsettled object if {@code value} is null
+     */
+    public static <T> Unsettled<T> of(@Nullable T value) {
+        //noinspection unchecked
+        return value == null ? (Unsettled<T>) NONE : new Unsettled<>(value, null);
+    }
+
+    /**
+     * Constructs a new unsettled object with a not successful result.
+     *
+     * @param thr the exception that was thrown during execution
+     * @param <T> the type of the expected result
+     * @return the newly constructed object
+     */
+    public static <T> Unsettled<T> thrown(@NotNull Throwable thr) {
+        return new Unsettled<>(null, Objects.requireNonNull(thr));
+    }
+
+    /**
+     * Executes a function. Any object thrown will be caught.
+     *
+     * @param func the function to execute
+     * @param <R>  the return type of the function
+     * @return an unsettled object containing the result or thrown object
+     */
+    public static <R> Unsettled<R> execute(Supplier<R> func) {
+        try {
+            return Unsettled.of(func.get());
+        } catch (Throwable t) {
+            return Unsettled.thrown(t);
+        }
+    }
+
+    protected final T value;
+    protected final Throwable thrown;
+
+    protected Unsettled(T value, Throwable thrown) {
+        this.value = value;
+        this.thrown = thrown;
+    }
+
+    /**
+     * @return {@code true} if the execution was a success and there is no thrown object present, {@code false} otherwise
+     */
+    public boolean wasSuccess() {
+        return thrown == null;
+    }
+
+    /**
+     * @return {@code true} if the execution was not successful and there is a thrown object present, {@code false} otherwise
+     */
+    public boolean wasFailure() {
+        return thrown != null;
+    }
+
+    /**
+     * @return an optional wrapping the actual value, may be empty
+     */
+    public Optional<T> get() {
+        return Optional.ofNullable(value);
+    }
+
+    /**
+     * @return an optional wrapping the thrown object, may be empty
+     */
+    public Optional<Throwable> getThrown() {
+        return Optional.ofNullable(thrown);
+    }
+
+}
