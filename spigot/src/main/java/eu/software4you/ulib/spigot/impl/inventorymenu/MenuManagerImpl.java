b6c75ccbd4be7399d22ce033b6ed8a090280961b
fluse1367 <fluse1367@software4you.eu>
2022-06-30 17:03:24 +0200

    redid ManuManager as interface

--- spigot/src/main/java/eu/software4you/ulib/spigot/impl/inventorymenu/MenuManagerImpl.java
+++ spigot/src/main/java/eu/software4you/ulib/spigot/impl/inventorymenu/MenuManagerImpl.java
@@ -1,22 +1,140 @@
 package eu.software4you.ulib.spigot.impl.inventorymenu;
 
+import eu.software4you.ulib.core.collection.Pair;
 import eu.software4you.ulib.spigot.impl.DelegationListener;
 import eu.software4you.ulib.spigot.inventorymenu.MenuManager;
+import eu.software4you.ulib.spigot.inventorymenu.menu.*;
+import lombok.Synchronized;
+import org.bukkit.event.*;
 import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public final class MenuManagerImpl extends MenuManager {
+import java.util.*;
+
+public final class MenuManagerImpl implements MenuManager {
+
+    private final Plugin plugin;
+    private final MenuManagerListener handler;
+    private final List<Menu> menus = new LinkedList<>();
+
+    private Listener dummyListener;
+    private boolean listening = false;
 
     public MenuManagerImpl(Plugin plugin) {
-        super(plugin);
+        this.plugin = plugin;
+        this.handler = new MenuManagerListener(this);
+    }
 
-        var listener = new MenuManagerListener(this);
-        super.handlerRegister = () -> {
-            var d = DelegationListener.registerSingleDelegation(InventoryClickEvent.class, listener::handle);
-            DelegationListener.registerDelegation(d, InventoryOpenEvent.class, listener::handle);
-            DelegationListener.registerDelegation(d, InventoryCloseEvent.class, listener::handle);
-            return d;
-        };
-        super.reset = () -> listener.getNoTriggerOpenClose().clear();
+    private void registerHandles() {
+        DelegationListener d = new DelegationListener();
+        DelegationListener.registerDelegation(d, InventoryClickEvent.class, handler::handle,
+                EventPriority.NORMAL, false, plugin);
+        DelegationListener.registerDelegation(d, InventoryOpenEvent.class, handler::handle,
+                EventPriority.NORMAL, false, plugin);
+        DelegationListener.registerDelegation(d, InventoryCloseEvent.class, handler::handle,
+                EventPriority.NORMAL, false, plugin);
+        this.dummyListener = d;
+    }
+
+    private void reset() {
+        handler.getNoTriggerOpenClose().clear();
+    }
+
+    @Override
+    @Synchronized
+    public void listen() {
+        if (listening)
+            return;
+        registerHandles();
+        reset();
+        listening = true;
+    }
+
+    @Override
+    @Synchronized
+    public void stopListening() {
+        if (!listening)
+            return;
+        HandlerList.unregisterAll(dummyListener);
+        reset();
+        listening = false;
+    }
+
+    @Override
+    public void registerMenu(@NotNull Menu menu) {
+        menus.add(menu);
+    }
+
+    @Override
+    public void unregisterMenu(@NotNull Menu menu) {
+        menus.remove(menu);
+    }
+
+    @Override
+    public @Nullable Page getPage(@NotNull Inventory inventory) {
+        for (Menu m : menus) {
+            if (m instanceof Page page) {
+                if (page.getInventory().equals(inventory))
+                    return page;
+
+                continue;
+            }
+
+            if (!(m instanceof MultiPageMenu menu))
+                continue;
+
+            for (Map.Entry<Integer, Page> entry : menu.getPages().entrySet()) {
+                Page page = entry.getValue();
+                if (page.getInventory().equals(inventory))
+                    return page;
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Menu getMenu(@NotNull Page page) {
+        if (page instanceof SinglePageMenu)
+            return (SinglePageMenu) page;
+
+        for (Menu m : menus) {
+            if (!(m instanceof MultiPageMenu menu)) {
+                continue;
+            }
+
+            if (menu.getPages().values().stream()
+                    .anyMatch(page::equals))
+                return menu;
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Menu getMenu(@NotNull Inventory inventory) {
+        var page = getPage(inventory);
+        return page == null ? null : getMenu(page);
+    }
+
+    @Override
+    public @Nullable Pair<MultiPageMenu, Integer> tryPage(@NotNull Page page) {
+        for (Menu m : menus) {
+            if (!(m instanceof MultiPageMenu menu))
+                continue;
+
+            var op = menu.getPages().entrySet().stream()
+                    .filter(e -> e.getValue().equals(page))
+                    .findFirst()
+                    .map(Map.Entry::getKey)
+                    .map(i -> new Pair<>(menu, i));
+
+            if (op.isPresent())
+                return op.get();
+        }
+
+        return null;
     }
 }
--- spigot/src/main/java/eu/software4you/ulib/spigot/inventorymenu/MenuManager.java
+++ spigot/src/main/java/eu/software4you/ulib/spigot/inventorymenu/MenuManager.java
@@ -3,32 +3,16 @@ package eu.software4you.ulib.spigot.inventorymenu;
 import eu.software4you.ulib.core.collection.Pair;
 import eu.software4you.ulib.spigot.impl.inventorymenu.MenuManagerImpl;
 import eu.software4you.ulib.spigot.inventorymenu.menu.*;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
-import java.util.function.Supplier;
-
 /**
  * Manages {@link Menu} instances.
  * <p>You have to call {@link #listen()} in order to make the manager effective.
  */
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-// TODO: redesign as interface
-public abstract class MenuManager {
-
-    private final Plugin plugin;
-    private final List<Menu> menus = new ArrayList<>();
-    private Listener handler;
-    protected Supplier<Listener> handlerRegister;
-    protected Runnable reset;
-    private boolean listening = false;
+public interface MenuManager {
 
     /**
      * Creates a new menu manager.
@@ -37,31 +21,19 @@ public abstract class MenuManager {
      * @return the newly created menu manager
      */
     @NotNull
-    public static MenuManager of(@NotNull Plugin plugin) {
+    static MenuManager of(@NotNull Plugin plugin) {
         return new MenuManagerImpl(plugin);
     }
 
     /**
      * Starts listening for the bukkit events in order to manage the menus. Effectively starts the manager.
      */
-    public void listen() {
-        if (listening)
-            return;
-        this.handler = handlerRegister.get();
-        reset.run();
-        listening = true;
-    }
+    void listen();
 
     /**
      * Stops listening for the bukkit events. Effectively stops the manager.
      */
-    public void stopListening() {
-        if (!listening)
-            return;
-        HandlerList.unregisterAll(handler);
-        reset.run();
-        listening = false;
-    }
+    void stopListening();
 
     /**
      * Registers a new menu that should be managed.
@@ -69,18 +41,14 @@ public abstract class MenuManager {
      *
      * @param menu the menu that should be managed
      */
-    public void registerMenu(@NotNull Menu menu) {
-        menus.add(menu);
-    }
+    void registerMenu(@NotNull Menu menu);
 
     /**
      * Deletes a menu registration.
      *
      * @param menu the menu whose registration should be deleted
      */
-    public void unregisterMenu(@NotNull Menu menu) {
-        menus.remove(menu);
-    }
+    void unregisterMenu(@NotNull Menu menu);
 
     /**
      * Gets the respective page to an {@link Inventory}.
@@ -89,21 +57,7 @@ public abstract class MenuManager {
      * @return the page, or {@code null} of nothing found
      */
     @Nullable
-    public Page getPage(@NotNull Inventory inventory) {
-        for (Menu m : menus) {
-            if (m instanceof MultiPageMenu menu) {
-                for (Map.Entry<Integer, Page> entry : menu.getPages().entrySet()) {
-                    Page page = entry.getValue();
-                    if (page.getInventory().equals(inventory))
-                        return page;
-                }
-            } else if (m instanceof Page page) {
-                if (page.getInventory().equals(inventory))
-                    return page;
-            }
-        }
-        return null;
-    }
+    Page getPage(@NotNull Inventory inventory);
 
     /**
      * Gets the respective menu to an {@link Page}
@@ -112,20 +66,7 @@ public abstract class MenuManager {
      * @return the menu, or {@code null} of nothing found
      */
     @Nullable
-    public Menu getMenu(@NotNull Page page) {
-        if (page instanceof SinglePageMenu)
-            return (SinglePageMenu) page;
-
-        for (Menu m : menus) {
-            if (m instanceof MultiPageMenu menu) {
-                for (Map.Entry<Integer, Page> entry : menu.getPages().entrySet()) {
-                    if (entry.getValue().equals(page))
-                        return menu;
-                }
-            }
-        }
-        return null;
-    }
+    Menu getMenu(@NotNull Page page);
 
     /**
      * Gets the respective menu to an {@link Inventory}
@@ -134,10 +75,7 @@ public abstract class MenuManager {
      * @return the menu, or {@code null} of nothing found
      */
     @Nullable
-    public Menu getMenu(@NotNull Inventory inventory) {
-        var page = getPage(inventory);
-        return page == null ? null : getMenu(page);
-    }
+    Menu getMenu(@NotNull Inventory inventory);
 
     /**
      * Attempts to find a page within a {@link MultiPageMenu}.
@@ -146,15 +84,5 @@ public abstract class MenuManager {
      * @return the respective {@link MultiPageMenu} and the page index, or {@code null} of nothing found
      */
     @Nullable
-    public Pair<MultiPageMenu, Integer> tryPage(@NotNull Page page) {
-        for (Menu m : menus) {
-            if (m instanceof MultiPageMenu menu) {
-                for (Map.Entry<Integer, Page> entry : menu.getPages().entrySet()) {
-                    if (entry.getValue().equals(page))
-                        return new Pair<>(menu, entry.getKey());
-                }
-            }
-        }
-        return null;
-    }
+    Pair<MultiPageMenu, Integer> tryPage(@NotNull Page page);
 }
