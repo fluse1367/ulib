7ea58c09085d04a55e69cf619e68e9b0277988b2
fluse1367 <fluse1367@software4you.eu>
2022-07-14 16:38:47 +0200

    privileging plugins pre construction

--- loader/src/main/java/eu/software4you/ulib/loader/minecraft/PluginSpigot.java
+++ loader/src/main/java/eu/software4you/ulib/loader/minecraft/PluginSpigot.java
@@ -1,22 +1,63 @@
 package eu.software4you.ulib.loader.minecraft;
 
 import eu.software4you.ulib.core.inject.*;
+import eu.software4you.ulib.core.reflect.ReflectUtil;
 import eu.software4you.ulib.loader.impl.EnvironmentProvider;
 import eu.software4you.ulib.loader.impl.init.InitAccess;
 import eu.software4you.ulib.loader.install.Installer;
 import lombok.SneakyThrows;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 
 public class PluginSpigot extends JavaPlugin {
     static {
         EnvironmentProvider.initAs(EnvironmentProvider.Environment.SPIGOT);
         Installer.installMe();
+    }
+
+    public PluginSpigot() {
+        // Spigot based servers use a separate class loader for each plugin, so we need to privilege each plugin that requires ulib individually.
+        try {
+            new HookInjection()
+                    // privilege at plugin pre-construct
+                    .addHook(ReflectUtil.forName("org.bukkit.plugin.java.PluginClassLoader", true, Bukkit.class.getClassLoader()).orElseRethrowRE(),
+                            "<init>",
+                            InjectUtil.createHookingSpec(HookPoint.METHOD_CALL, "Ljava/lang/Class;forName(Ljava/lang/String;ZLjava/lang/ClassLoader;)Ljava/lang/Class;"),
+                            (__, cb) -> {
+
+                                ClassLoader self = (ClassLoader) cb.self().orElseThrow();
+                                var desc = ReflectUtil.icall(PluginDescriptionFile.class,
+                                        self, "description").orElseRethrow();
+
+                                filter(desc, () -> InitAccess.getInstance().privileged(self, true));
+                            })
+
+                    // un-privilege at plugin post-disable
+                    .addHook(JavaPluginLoader.class.getMethod("disablePlugin", Plugin.class),
+                            InjectUtil.createHookingSpec(HookPoint.METHOD_CALL, "Lorg/bukkit/plugin/java/JavaPlugin;setEnabled(Z)V"),
+                            (__, cb) -> {
+                                Plugin pl = (Plugin) cb.proxyInst().orElseThrow();
+                                filter(pl.getDescription(), () -> InitAccess.getInstance().privileged(pl.getClass().getClassLoader(), false));
+                            })
+
+                    .inject()
+                    .rethrowRE();
+        } catch (NoSuchMethodException e) {
+            throw new InternalError(e);
+        }
+    }
+
+    private void filter(PluginDescriptionFile desc, Runnable onSuccess) {
+        if (!desc.getDepend().contains(getName()) && !desc.getSoftDepend().contains(getName()))
+            return;
+
+        onSuccess.run();
     }
 
     private Plugin pluginSubstitute;
@@ -26,36 +67,6 @@ public class PluginSpigot extends JavaPlugin {
     public void onLoad() {
         this.pluginSubstitute = (Plugin) InitAccess.getInstance().construct("spigot", "eu.software4you.ulib.spigot.impl.PluginSubst",
                 this, getPluginLoader(), getDescription(), getDataFolder(), getFile());
-
-        // Spigot based servers use a separate class loader for each plugin, so we need to privilege each plugin that requires ulib individually.
-        try {
-            var spec = InjectUtil.createHookingSpec(HookPoint.METHOD_CALL, "Lorg/bukkit/plugin/java/JavaPlugin;setEnabled(Z)V");
-            new HookInjection()
-                    // privilege at plugin pre-enable
-                    .addHook(JavaPluginLoader.class.getMethod("enablePlugin", Plugin.class), spec, (__, cb) ->
-                            filter(cb.proxyInst(), p -> InitAccess.getInstance().privileged(p.getClass().getClassLoader(), true)))
-
-                    // un-privilege at plugin post-disable
-                    .addHook(JavaPluginLoader.class.getMethod("disablePlugin", Plugin.class), spec, (__, cb) ->
-                            filter(cb.proxyInst(), p -> InitAccess.getInstance().privileged(p.getClass().getClassLoader(), false)))
-
-                    .inject()
-                    .rethrowRE();
-        } catch (NoSuchMethodException e) {
-            throw new InternalError(e);
-        }
-    }
-
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    private void filter(Optional<Object> pluginOpt, Consumer<Plugin> consumer) {
-        if (!(pluginOpt.orElse(null) instanceof Plugin plugin))
-            return;
-
-        var desc = plugin.getDescription();
-        if (!desc.getDepend().contains(getName()) && !desc.getSoftDepend().contains(getName()))
-            return;
-
-        consumer.accept(plugin);
     }
 
     @Override
